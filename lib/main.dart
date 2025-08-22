6dd40829668053ed098ed18fd7e2b00fdbe70127
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-08-22 18:15:17 +0300

    handel main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:whatsapp_clone_flutter/core/configs/themes/theme.dart';
-import 'package:whatsapp_clone_flutter/features/home/presentation/pages/home_screen.dart';
-import 'package:whatsapp_clone_flutter/firebase_options.dart';
+import 'package:whatsapp_clone_flutter/shared/services/firebase_options.dart';
+import 'package:whatsapp_clone_flutter/shared/widgets/whats_app.dart';
 
 void main() async {
+  // widget initialization
   WidgetsFlutterBinding.ensureInitialized();
+  // Firebase initialization
   await Firebase.initializeApp(
   options: DefaultFirebaseOptions.currentPlatform,
 );
+  // Configuring Screen Size
   await ScreenUtil.ensureScreenSize();
-
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // Initialize ScreenUtil for responsive UI
-    return ScreenUtilInit(
-      designSize: const Size(375, 812), // Standard design size
-      minTextAdapt: true,
-      splitScreenMode: true,
-      builder: (context, child) {
-        return MaterialApp(
-          title: 'WhatsApp Clone',
-          debugShowCheckedModeBanner: false,
-
-          theme: AppTheme.lightTheme(),
-          darkTheme: AppTheme.darkTheme(),
-          themeMode: ThemeMode.system, // Automatically adapt to system theme
-          home: child,
-        );
-      },
-      child: const HomePage(),
-    );
-  }
+  // App Lunch Point
+  runApp(const WhatsApp());
 }
