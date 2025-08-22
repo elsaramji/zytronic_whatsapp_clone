f360ec2913ee366458de6bdd3342df0ff9a8dead
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-08-22 05:00:38 +0300

    installation firebase by CLI ✅

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:whatsapp_clone_flutter/core/configs/themes/theme.dart';
 import 'package:whatsapp_clone_flutter/features/home/presentation/pages/home_screen.dart';
+import 'package:whatsapp_clone_flutter/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   await ScreenUtil.ensureScreenSize();
+
   runApp(const MyApp());
 }
 
